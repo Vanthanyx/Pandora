6cf14107e2643f98b90e7097bc28ae41bdf401c4
Vanthanyx <73613589+Vanthanyx@users.noreply.github.com>
2021-09-04 00:48:20 -0600

    Update ButtonFunction.bat

--- ButtonFunction.bat
+++ ButtonFunction.bat
@@ -1,4 +1,4 @@
-@Echo off
+@echo OFF
 @Mode 42,15
 @Title [ ... ]
 Batbox /h 0
@@ -7,7 +7,7 @@ Batbox /h 0
 Call Button  10 4 "INST" 24 4 "UNST" 17 8 "EXIT" # Press
 Getinput /m %Press% /h 70
 
-if %errorlevel%==1 (start "")
-if %errorlevel%==2 (start "")
+if %errorlevel%==1 (start "../../SRC/Batch/Admin/INST.bat")
+if %errorlevel%==2 (start "../../SRC/Batch/Admin/UNST.bat")
 if %errorlevel%==3 (exit)
 goto Loop

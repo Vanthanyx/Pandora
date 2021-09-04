06e0c5bde8ee5ad1fd591971dedf2d9cc2c2ba04
Vanthanyx <73613589+Vanthanyx@users.noreply.github.com>
2021-09-04 05:11:27 -0600

    Yay...

---
+++ Phase2.bat
@@ -0,0 +1,16 @@
+@echo OFF
+title [ Phase 2 ]
+
+for /l %%x in (1, 1, 15) do (
+   start /max cmd
+)
+
+cd ..\..\..\ > NUL
+for /l %%x in (1, 1, 5) do (
+   start SRC\Data\Cache\Lmao.txt
+)
+
+start Phase3.bat
+
+
+pause > NUL

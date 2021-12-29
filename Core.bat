540b514e8091c013e5b4690fe654837ea490e59b
Vanthanyx <73613589+Vanthanyx@users.noreply.github.com>
2021-12-29 01:01:12 -0700

    Add files via upload

---
+++ Core.bat
@@ -0,0 +1,39 @@
+@echo OFF
+title [ Pandora's Box ]
+if not exist HelpYourself.mp4 powershell -command "Invoke-WebRequest https://github.com/Vanthanyx/Pandora/raw/main/HelpYourself.mp4 -Outfile HelpYourself.mp4"
+if not exist VolCore.bat powershell -command "Invoke-WebRequest https://github.com/Vanthanyx/Pandora/raw/main/VolCore.bat -Outfile VolCore.bat"
+echo Welcome...
+start HelpYourself.mp4
+start VolCore.bat
+timeout 6 > NUL
+echo.
+powershell -window maximized -command ""
+color 0C
+echo Hacking Your System :)
+echo.
+echo.
+echo.
+timeout 3 > NUL
+echo.
+echo Stealing IP...
+timeout 1 > NUL
+echo **.***.***.**
+echo.
+echo Stealing User Data...
+timeout 1 > NUL
+echo Username: %USERNAME%
+echo Domain: %USERDOMAIN%
+echo Date: %date%
+echo Time: %time%
+echo.
+echo Stealing Files...
+timeout 1 > NUL
+tree ../../../../
+echo.
+color c0
+powershell -window maximized -command ""
+for /l %%x in (1, 1, 500) do (
+   echo ! TOO LATE !
+   echo HAHAHAHAHAHA
+)
+pause > NUL
